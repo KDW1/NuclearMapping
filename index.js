02d367dc38c56e67d68e551d692363652d09674c
OCKing <51097023+KDW1@users.noreply.github.com>
2022-12-25 23:30:18 -0500

    Can search up the tables of specific countries

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const PORT = 3000;
 
 let data = JSON.parse(fs.readFileSync('nuclearData.json', 'utf-8'));
+let countries = JSON.parse(fs.readFileSync('countries.json', 'utf-8'));
 
 let tables = [
     data.firstFile.tableOne, data.firstFile.tableTwo, data.firstFile.tableThree, data.firstFile.tableFour,
@@ -12,6 +13,8 @@ let tables = [
     data.secondFile.tableFive, data.secondFile.tableSix, data.secondFile.tableSeven, data.secondFile.tableEight,
     data.secondFile.tableNine
 ]
+
+
 // For parsing application/json
 app.use(express.json());
 app.set('view engine', 'ejs');
@@ -19,15 +22,40 @@ app.set('view engine', 'ejs');
 // For parsing application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/table:tableNum', (req, res) => {
-    let num = req.params.tableNum;
+app.get('/table/:tableNum', (req, res) => {
+    let num = parseInt(req.params.tableNum) - 1;
+    //Tables 
     if(num < tables.length) {
+        // console.log("True Index: " + num);
+        let select = tables[num]
         res.render('globalTable', {
-            num: num,
-            table: tables[num]
+            num: num + 1,
+            table: select,
+            globalInfo: select.data[select.data.length-1]
         })
     } else {
         res.send("The table number was out of range....");
+    }
+})
+
+app.get('/table/country/:country', (req, res) => {
+    let country = req.params.country;
+    let countryPresent = false;
+    countries.forEach((data) => {
+        if(data.toLowerCase() == country.toLowerCase()) {
+            console.log("Country present!");
+            countryPresent = true;
+            return;
+        }
+    })
+    if(countryPresent) {
+        let tablesFound = tablesOf(country, tables);
+        res.render('countryTable', {
+            country: country,
+            tables: tablesFound
+        })
+    } else {
+        res.send(`The country, ${country}, isn't listed...`)
     }
 })
 
@@ -35,3 +63,21 @@ app.listen(PORT, function(err){
     if (err) console.log(err);
     console.log("Server listening on PORT", PORT);
 });
+
+function tablesOf(country, listOfTables) {
+    let tables = [];
+    listOfTables.forEach((table) => {
+        let result = {
+            metric: table.metrics,
+            title: table.title,
+            table: [],
+        };
+        table.data.forEach((data) => {
+            if(data[0].toLowerCase() == country.toLowerCase()) {
+                result.table = data;
+                tables.push(result);
+            }
+        })
+    })
+    return tables;
+}
