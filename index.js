a751dc46710493edd4bb18de938d48528146cbd6
OCKing <51097023+KDW1@users.noreply.github.com>
2023-01-01 23:08:56 -0500

    Oof..

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require("fs");
-const { countReset } = require('console');
 const app = express(); 
 const port = 3000;
 
