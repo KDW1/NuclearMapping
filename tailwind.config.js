6cbd71685d62187a2192bff3cb739d3024d8de9f
OCKing <51097023+KDW1@users.noreply.github.com>
2023-01-01 21:59:19 -0500

    Responsive Design Changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,26 @@ module.exports = {
     './*/*.ejs'
   ],
   theme: {
+    screens: {
+      'bruh': '430px',
+
+      'sm': '550px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '675px',
+      // => @media (min-width: 768px) { ... }
+      'overflow-x': '740px',
+
+      'lg': '1050px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px'
+      // => @media (min-width: 1536px) { ... }
+    },
+    display: ['group-hover'],
     extend: {},
   },
   plugins: [
