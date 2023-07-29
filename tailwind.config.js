ab434fe0df3ba86ef05ccd309f00f8bf97f2f96d
King-Diorr Willsun <51097023+KDW1@users.noreply.github.com>
2023-07-28 23:35:16 -0500

    Successfully setting up transfer to Nextjs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,39 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-const colors = require('tailwindcss/colors');
-
 module.exports = {
   content: [
-    './**/*.ejs'
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    screens: {
-      'bruh': '430px',
-
-      'sm': '550px',
-      // => @media (min-width: 640px) { ... }
-
-      'md': '675px',
-      // => @media (min-width: 768px) { ... }
-      'overflow-x': '740px',
-
-      'lg': '1050px',
-      // => @media (min-width: 1024px) { ... }
-
-      'xl': '1280px',
-      // => @media (min-width: 1280px) { ... }
-
-      '2xl': '1536px'
-      // => @media (min-width: 1536px) { ... }
-    },
-    display: ['group-hover'],
     extend: {
-      colors: {
-        main: colors.rose[700],
-        shade: colors.red[900],
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/line-clamp'),
-  ],
+  plugins: [],
 }
